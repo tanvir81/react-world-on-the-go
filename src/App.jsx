f13e7d5eacf2de4146e1b6242089a464b2bb4680
Tanvir <tkdarkshadow0@gmail.com>
2025-09-20 07:49:27 +0600

    World-on go-first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/countries/countries";
+
+function App() {
+  return (
+    <>
+      <h1>React World On The Go</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
